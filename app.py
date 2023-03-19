37bdc47645ef7a73b2cc29a949f4e239d34753c7
kjszk5 <kjszk5@gmail.com>
2023-03-19 23:36:48 +0900

    html first commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory,session
 import azure.cognitiveservices.speech as speechsdk
+import requests
 import os
+import script_get
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'masterkey'
 
 @app.route('/')
 def index():
@@ -19,45 +22,60 @@ def favicon():
 def wav():
    return render_template('wav.html')
 
-@app.route('/hello', methods=['POST'])
+@app.route('/script_view',methods=['GET'])
+def script_view():
+   page = int(request.args.get("page"))
+   print(page)
+
+   en_script,jp_script,page = script_get.script_get(request.args)
+   session['en_script'] = en_script
+   return render_template('script_view.html',page=page+1, en_script=en_script, jp_script=jp_script)
+
+
+@app.route('/score',methods=['GET','POST'])
+def score():
+   if request.method == 'GET':
+      print("GET")
+#      print(request.form)
+      print("GET END")
+   else:
+      print("POST")
+      data = request.data
+      with open('./static/aaa.wav','wb') as f:
+         f.write(data)
+      audioFile = './static/aaa.wav'
+      print("POST END")
+      COG_SERVICE_KEY="f475a189ffdd4362bfa09715ebc73660"
+      COG_SERVICE_REGION="japaneast"
+      config = speechsdk.SpeechConfig(subscription = COG_SERVICE_KEY,region = COG_SERVICE_REGION)
+      reference_text = "May I help you?"
+      audio_config = speechsdk.AudioConfig(filename=audioFile)
+      pronunciation_config = speechsdk.PronunciationAssessmentConfig(reference_text=reference_text,grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,enable_miscue=True)
+      print("TEST4")
+      try:
+   #       speech_recognizer = speechsdk.SpeechRecognizer(speech_config=config)
+         speech_recognizer = speechsdk.SpeechRecognizer(speech_config=config,audio_config=audio_config)
+         pronunciation_config.phoneme_alphabet = 'IPA'
+         print("TEST2")
+         pronunciation_config.apply_to(speech_recognizer)
+         print("TEST3")
+         result = speech_recognizer.recognize_once()
+         print("TEST4")
+         pronunciation_result = speechsdk.PronunciationAssessmentResult(result)
+         print("TEST5")
+         score = [pronunciation_result.accuracy_score,]
+         print(pronunciation_result.accuracy_score)
+      except Exception as ex:
+         print("RECOGNIZE ERROR")
+      print("TEST END")
+
+#   name = request.form['key1']
+#   print(name)
+   return render_template('score.html')
+
+@app.route('/hello', methods=['GET','POST'])
 def hello():
    name = request.form.get('name')
-   COG_SERVICE_KEY="f475a189ffdd4362bfa09715ebc73660"
-   COG_SERVICE_REGION="japaneast"
-   config = speechsdk.SpeechConfig(subscription = COG_SERVICE_KEY,region = COG_SERVICE_REGION)
-#   config.speech_recognition_language="en-US"
-#   config.enable_dictation()
-#   audio_config = speechsdk.AudioConfig(use_default_microphone=True)
-   print('speech serviceのregionはこちらに設定しました:', config.region)
-   reference_text="I got a cold call from an insurance company yesterday."
-   audioFile = 'bbb.wav'
-   audio_config = speechsdk.AudioConfig(filename=audioFile)
-#   audio_config = speechsdk.AudioConfig(use_default_microphone=True)
-
-   pronunciation_config = speechsdk.PronunciationAssessmentConfig(reference_text=reference_text,grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,enable_miscue=True)
-   print("TEST1")
-   print(os.path.abspath(__file__))
-   print(os.path.abspath(audioFile))
-   try:
-#       speech_recognizer = speechsdk.SpeechRecognizer(speech_config=config)
-       speech_recognizer = speechsdk.SpeechRecognizer(speech_config=config,audio_config=audio_config)
-       pronunciation_config.phoneme_alphabet = 'IPA'
-       print("TEST2")
-       pronunciation_config.apply_to(speech_recognizer)
-       print("TEST3")
-       result = speech_recognizer.recognize_once()
-       print("TEST4")
-       print(result.text)
-       pronunciation_result = speechsdk.PronunciationAssessmentResult(result)
-       print(pronunciation_result.accuracy_score)
-       print("TEST5")
-       script = result.text
-       score = pronunciation_result.accuracy_score
-
-   except Exception as ex:
-       print("RECOGNIZE ERROR")
-       print("Speech recognition failed: {}".format(ex))
-
    if name:
        print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name, script = script, score = score)
