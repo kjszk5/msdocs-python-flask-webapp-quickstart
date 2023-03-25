9f6402ffbd261c5fe82c42a2499360c16c1c1a1e
kjszk5 <kjszk5@gmail.com>
2023-03-25 23:26:20 +0900

    1問目のみ処理するため、問題数による処理を削除

--- script_get.py
+++ script_get.py
@@ -7,29 +7,20 @@ def script_get(request_args):
     print(request_args)
     print(request_args["question_no"])
     question_no = int(request_args["question_no"])
-    if question_no == 0:
-        df = pd.read_csv("script.csv")
-        df['accuracy_score']=0
-        df['fluency_score']=0
-        df['completeness_score']=0
-        df['pronunciation_score']=0
+    question_num = int(session["question_num"])
+    df = pd.read_csv("script.csv")
+    df['accuracy_score']=0
+    df['fluency_score']=0
+    df['completeness_score']=0
+    df['pronunciation_score']=0
 
-        script_list = df.to_numpy().tolist()
-        random.shuffle(script_list)
-        print(script_list[0][0])
-        en_script = script_list[0][0]
-        jp_script = script_list[0][1]
-        session["script_list"] = script_list[:10]
-        session["question_no"] = question_no
-    else:
-        script_list = session["script_list"] 
-        print("Script_GET")
-#        print(script_list)
-        
-        en_script = script_list[question_no][0]
-        jp_script = script_list[question_no][1]
-        session["question_no"] = question_no
-
+    script_list = df.to_numpy().tolist()
+    random.shuffle(script_list)
+    print(script_list[0][0])
+    en_script = script_list[0][0]
+    jp_script = script_list[0][1]
+    session["script_list"] = script_list[:question_num]
+    session["question_no"] = question_no
 
     return en_script,jp_script,question_no
 
